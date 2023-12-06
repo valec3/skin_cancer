83bd918bb4bd44f1279ebeadf2230ca2fa3e72c1
Noe Ulises Machaca Chambilla <115515737+ratatouille99@users.noreply.github.com>
2023-12-06 03:32:52 +0000

    Mensaje descriptivo de los cambios realizados

--- model.py
+++ model.py
@@ -7,6 +7,16 @@ import numpy as np
 # Carga el modelo
 model = load_model("modelo/model_a.h5")
 
+predicciones = {
+    1 : "Queratosis actínica y carcinoma intraepitelial",
+    2 : "Carcinoma de células basales",
+    3 : "Lesiones benignas similares a queratosis",
+    4 : "Dermatofibroma",
+    5 : "Melanoma",
+    6 : "Nevos melanocíticos",
+    7 : "Lesiones vasculares",
+}
+
 def predict_image(img):
     # Preprocesar la imagen para que coincida con las dimensiones de entrada del modelo
     img = img.resize((250, 250))  # Ajusta el tamaño según la configuración del modelo
@@ -20,4 +30,4 @@ def predict_image(img):
     print("El valor máximo es:", valor_maximo)
     print("El índice del valor máximo es:", indice_maximo)
     
-    return indice_maximo
+    return predicciones[indice_maximo]
